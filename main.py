e29e0b49ee02a1e786dd34d8684f41e5b8d0a274
cat <31978757+cy215482@users.noreply.github.com>
2019-07-27 19:52:46 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:passwordlol@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
